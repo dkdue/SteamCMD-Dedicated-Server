9d9a57d4588091fc5cf1a56356ee213bdfab109b
kessef <dkdue@hotmail.com>
2021-09-15 21:42:27 +0200

    Discord Webhook
    Basic messages webhook

---
+++ SteamCMD.ps1
@@ -0,0 +1,143 @@
+$Config = Get-Content "C:\SteamCMD\server.config" | convertfrom-json
+$host.UI.RawUI.WindowTitle = "SteamCMD Dedicated Server Watchdog"
+Write-Host "SteamCMD Dedicated Server" -ForegroundColor DarkGreen -BackgroundColor Black
+write-host "-------------------------------"
+Start-Sleep -Seconds 1.5
+########################################################################################################################################################
+if (-not (Test-Path ".\SteamCMD")) {
+#Creating Server Folder
+write-host "Creating Server Folder"
+$Dir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+New-item $Dir -type Directory -ErrorAction SilentlyContinue
+
+#Creating Steam Folder and Downloading SteamCMD
+Write-Host "Downloading SteamCMD"
+$url = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
+New-item "$Dir\SteamCMD" -type Directory -ErrorAction SilentlyContinue
+$output =  "$Dir\SteamCMD\steamcmd.zip"
+$start_time = Get-Date
+Invoke-WebRequest -Uri $url -OutFile $output
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+#Expanding SteamCMD.zip (Open the zip file and extract)
+$SteamZip = "$Dir\SteamCMD\steamcmd.zip" #the file name to extract
+$SteamDestination = "$Dir\SteamCMD" #the path to extract the file to
+Remove-Item $Dir\SteamCMD\steamcmd.exe -ErrorAction SilentlyContinue #Just in cases this was alredy run once, I don´t want to flood the uers text
+Add-Type -assembly "system.io.compression.filesystem" #Required class to unzip the file
+[io.compression.zipfile]::ExtractToDirectory($SteamZip, $SteamDestination) #unzip the file
+
+Write-Host "The Server is going to be installed to $Dir" -ForegroundColor Yellow
+Write-Host "Giving you 5 seconds to change your mind" -ForegroundColor Yellow
+sleep -Seconds 5
+
+#Installing the Server
+Write-Host "Installing The Server. This will take a WHILE..." -ForegroundColor Yellow
+powershell.exe "$Dir\SteamCMD\steamcmd.exe" +login anonymous +force_install_dir $($config.forceinstalldir) +app_update $($config.gameid) validate +exit 
+}
+########################################################################################################################################################
+## Function for sending messages via Discord
+$debug = $false
+function SendDiscordMessage($message, $toAll = $true){
+    if(!$debug){
+        
+        if($toAll){
+            $message = "@everyone $message"
+        }
+        Invoke-RestMethod -Uri $($config.webHookUrl) -Body ([PSCustomObject]@{content = $message} | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
+    }
+}
+Function Start-Server {
+    #Starts the Server Server
+    $Process = get-process $($config.PIDname) -ErrorAction SilentlyContinue
+    if ($Process){
+        write-host "Server is running.."
+    }else {
+		SendDiscordMessage -message "**[START-SERVER]**`nStarting the server $($config.servername)..$(Get-Date)`nhttps://"
+        write-host "Starting the server $($config.servername).. "
+		Start-Process "$($config.forceinstalldir)\$($config.ExeName)" -ArgumentList "$($config.ArgumentList)" 
+    }
+}
+Function Update-Server {
+    #Starts updating the Server Server
+    $Process = get-process $($config.PIDname) -ErrorAction SilentlyContinue
+    if ($Process){
+        write-host "Stop the game server first: Stop-Server"
+    }else {
+        Write-Host "Updating $($config.servername)"
+        Start-Process "$($config.steamcmd)" -ArgumentList "+login anonymous +force_install_dir $($config.forceinstalldir) +app_update $($config.gameid) validate +exit" -wait
+		SendDiscordMessage -message "**[UPDATE-SERVER]**`nUpdating $($config.servername) done!`nhttps://"
+    }
+}
+Function Stop-Server {
+    #Sends Ctrl+C to the Server window, which saves the server first and shuts down cleanly
+    $Process = get-process $($config.PIDname) -ErrorAction SilentlyContinue
+    if ($Process){
+        $MemberDefinition = '
+        [DllImport("kernel32.dll")]public static extern bool FreeConsole();
+        [DllImport("kernel32.dll")]public static extern bool AttachConsole(uint p);
+        [DllImport("kernel32.dll")]public static extern bool GenerateConsoleCtrlEvent(uint e, uint p);
+        public static void SendCtrlC(uint p) {
+            FreeConsole();
+            AttachConsole(p);
+            GenerateConsoleCtrlEvent(0, p);
+            FreeConsole();
+            AttachConsole(uint.MaxValue);
+        }'
+        Add-Type -Name 'dummyName' -Namespace 'dummyNamespace' -MemberDefinition $MemberDefinition
+        [dummyNamespace.dummyName]::SendCtrlC($Process.ID)
+		SendDiscordMessage -message "**[STOP-SERVER]**`nStopping $($config.servername) done!`nhttps://"
+    } else {
+        write-host "no process found, not terminating anything"
+    }
+}
+Function Get-ServerCurrentVersion {
+    ((Get-Content "$($config.forceinstalldir)\steamapps\appmanifest_$($config.gameid).acf" | Where-Object {$_ -like "*buildid*"}).split('"').trim() | Where-Object {$_})[-1]
+}
+Function Get-ServerLatestVersion {
+    Write-Host "Checking Steam API for latest version..."
+    $Data = Invoke-WebRequest -Uri "https://api.steamcmd.net/v1/info/$($config.gameid)" -UseBasicParsing
+    $json = $data.content | convertfrom-json
+    $BuildID = $json.data.$($config.gameid).depots.branches.public.buildid
+    $Status = $json.status
+    
+    if ($Status -eq 'success') {
+    Write-Host "Status: Success!" -ForegroundColor DarkGreen
+    } else {
+    Write-Host "Error, unable to contact the Steam services. Trying again later.." -ForegroundColor Red    
+    }
+
+
+Return $BuildID
+
+Start-Sleep -Seconds 1.5
+
+}
+#Run every 300 seconds forever
+$stop = "$false"
+do{
+    $BuildID = Get-ServerLatestVersion
+    $CurrentBuildID = Get-ServerCurrentVersion
+    
+    if ($BuildID -ne $CurrentBuildID){
+        #New version detected. Initiating patching
+        Stop-Server
+        Update-Server
+    } else {
+        Write-host "The server has the latest update: $($BuildID)"
+       
+    }
+    #This will start Server after patching, and even if it's not patched but crashed for some reason
+    Start-Server
+    #Will run every 5 minutes (300 seconds)
+    #Start-Sleep -Seconds 900
+$x = 1*60
+$length = $x / 100
+while($x -gt 0) {
+  $min = [int](([string]($x/60)).split('.')[0])
+  $text = " " + $min + " minutes " + ($x % 60) + " seconds remaining"
+  Write-Progress "The server has the latest update: $($BuildID), Next check ..." -status $text -perc ($x/$length)
+  start-sleep -s 1
+  $x--
+}
+} while ($stop)
+
